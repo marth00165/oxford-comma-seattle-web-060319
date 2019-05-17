157459a0bfd39fdc7855d363fbbe2bae99b51637
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-17 01:06:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.length == 1 
+  return array[0]
+elsif array.length == 2
+  array.join(" and ")
+else
+  new_ending = "and " + array.last 
+  array.pop
+  array << new_ending
+  array.join (", ")
+end
+
+
 
 end
